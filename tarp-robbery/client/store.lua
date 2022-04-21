eb72f18325b4f9937bafa0d3f427f484f8b536d3
trutzzz <scmvaam@gmail.com>
2022-04-22 00:49:10 +0700

    robbery
    tarp-robbery

---
+++ tarp-robbery/client/store.lua
@@ -0,0 +1,96 @@
+local robtime = 120 -- Time to rob (in seconds) now its 3.3mins
+local timerCount = robtime
+local isRobbing = false
+local timers = false
+local storetimer = nil
+local storenumber = nil
+
+
+RegisterNetEvent("Witness:ToggleNotification2")
+AddEventHandler("Witness:ToggleNotification2", function(coords, alert)
+	--print('store name '..tostring(alert))
+	TriggerEvent("vorp:TipBottom", 'Telegram of Robbery in Progress at ' .. alert, 15000)
+	local blip = Citizen.InvokeNative(0x45f13b7e0a15c880, -1282792512, coords.x, coords.y, coords.z, 15.0)
+	Wait(90000)--Time till notify blips dispears, 1 min
+	RemoveBlip(blip)
+end)
+
+--[[RegisterNetEvent("tarp-robbery:information1")
+AddEventHandler("tarp-robbery:information1", function(coords, alert)
+	print('perampokan notif')
+	TriggerEvent("vorp:TipBottom", 'perampokan sedang berlangsung | ' .. alert, 15000)
+	local blip = Citizen.InvokeNative(0x45f13b7e0a15c880, -1282792512, coords.x, coords.y, coords.z, 50.0)
+	Wait(90000)--Time till notify blips dispears, 1 min
+	RemoveBlip(blip)
+end)]]--
+
+function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+    local str = CreateVarString(10, "LITERAL_STRING", str)
+    SetTextScale(w, h)
+    SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+	SetTextCentre(centre)
+    if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+	Citizen.InvokeNative(0xADA9255D, 1);
+    DisplayText(str, x, y)
+end
+
+Citizen.CreateThread(function()
+    while true do
+    Citizen.Wait(0)
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
+        for i = 1, #Config.Toko do
+           currentshop =  i
+           if GetDistanceBetweenCoords(coords, Config.Toko[currentshop].coords.x, Config.Toko[currentshop].coords.y, Config.Toko[currentshop].coords.z, true)  < 1.2 then
+                DrawTxt("Press [~e~G~q~] to break into Register", 0.50, 0.95, 0.7, 0.7, true, 255, 255, 255, 255, true)
+                if IsControlJustReleased(0, 0x760A9C6F) then
+                    TriggerServerEvent("tarp-robbery:awalperampokan", function()
+                end)
+                Wait(5000)
+                local alert = Config.Toko [i].name
+                TriggerServerEvent("tarp-robbery:policeinfo", GetPlayers(), coords, alert, i) --Getting the item lockpick
+                Wait(0)
+                isRobbing = true   
+				end    
+           end
+        end
+    end
+end)
+
+function GetPlayers()
+    local players = {}
+
+    for i = 0, 31 do
+        if NetworkIsPlayerActive(i) then
+            table.insert(players, GetPlayerServerId(i))
+        end
+    end
+
+    return players
+end
+
+RegisterNetEvent("tarp-robbery:memulaiperampokan")
+AddEventHandler("tarp-robbery:memulaiperampokan", function()
+    local _source = source
+	local playerPed = PlayerPedId()
+	local coords = GetEntityCoords(playerPed)
+    local testplayer = exports["tarp-skillbar"]:taskBar(4000,7)
+
+    --jam = GetClockHours()
+    if GetClockHours() > 20 or GetClockHours() < 5 then
+        Wait(1000)
+        if testplayer == 100 then
+            TaskStartScenarioInPlace(playerPed, GetHashKey('world_human_shop_browse_counter'), 5000, true, false, false, false)
+            exports['progressBars']:startUI(5000, "Getting The Loot...")
+            Citizen.Wait(1000)
+            Citizen.Wait(5000)
+            ClearPedTasksImmediately(PlayerPedId())
+            ClearPedSecondaryTask(PlayerPedId())
+            Citizen.Wait(1000)
+            TriggerServerEvent("tarp-robbery:pendapatan", function(playerPed, coords)
+            end)   
+        end
+    else
+        exports['tarp-notify']:SendAlert('inform', ' tidak dapat melakukan aksi')
+    end
+end)
---
+++ tarp-robbery/server/store.lua
@@ -0,0 +1,74 @@
+VORP = exports.vorp_inventory:vorp_inventoryApi()
+
+data = {}
+TriggerEvent("vorp_inventory:getData",function(call)
+    data = call
+end)
+
+local VorpCore = {}
+
+TriggerEvent("getCore",function(core)
+    VorpCore = core
+end)
+
+RegisterNetEvent("tarp-robbery:awalperampokan")
+AddEventHandler("tarp-robbery:awalperampokan", function(robtime)
+    local _source = source
+    local Character = VorpCore.getUser(source).getUsedCharacter
+    local count = VORP.getItemCount(_source, "lockpick")
+
+    local pol = 0
+    if Character.job == "police" then 
+        pol = pol + 1    
+    end
+
+    if pol >= 1 then 
+        if count >= 1 then
+            VORP.subItem(_source,"lockpick", 1)
+            TriggerClientEvent('tarp-robbery:memulaiperampokan', _source)
+            TriggerClientEvent('tarp-notify:client:SendAlert', _source, { type = 'inform', text = "seseorang memanggil sheriff" })
+            TriggerClientEvent('tarp-notify:client:SendAlert', _source, { type = 'inform', text = "sheriff akan datang dalam waktu 1 menit" })
+        else
+            TriggerClientEvent('tarp-notify:client:SendAlert', _source, { type = 'inform', text = "kamu tidak mempunyai alat untuk merampok" })
+        end
+    else
+        TriggerClientEvent('tarp-notify:client:SendAlert', _source, { type = 'inform', text = "jumlah police tidak mencukupi" })
+    end
+end)
+
+RegisterNetEvent("tarp-robbery:policeinfo")
+AddEventHandler("tarp-robbery:policeinfo", function(players, coords, alert)
+    local Character = VorpCore.getUser(source).getUsedCharacter
+    for each, player in ipairs(players) do
+        if Character ~= nil then
+			if Character.job == 'police' or Character.job == 'marshal' or Character.job == 'sheriff' then
+				TriggerClientEvent("Witness:ToggleNotification2", player, coords, alert)
+			end
+            --TriggerClientEvent('tarp-notify:client:SendAlert', -1, { type = 'inform', text = "perampokan di daerah "..alert })
+        end
+    end
+end)
+
+RegisterNetEvent("tarp-robbery:pendapatan")
+AddEventHandler("tarp-robbery:pendapatan", function()
+    TriggerEvent('vorp:getCharacter', source, function(user)
+        local _source = source
+        local _user = user
+       -- randommoney = math.random(10,20)
+        ritem = math.random(5,10)
+        local randomitempull = math.random(1, #Config.Items)
+        local itemName = Config.Items[randomitempull]
+        VORP.addItem(_source, itemName, ritem)
+        TriggerClientEvent('tarp-notify:client:SendAlert', _source, { type = 'inform', text = "kamu mendapatkan item ketika membobol"})
+    end)    
+end)
+
+Locations = {
+    vector3(-324.26, 804.1, 117.93),
+    vector3(2825.2976074219, -1320.1049804688, 45.755676269531),
+    vector3(-785.47, -1323.85, 43.9),
+    vector3(-1789.34, -387.5, 160.37),
+    vector3(-3687.2, -2622.31, -13.3), 
+    vector3(-5486.33, -2937.6, -0.35),
+    vector3(1328.03, -1293.70,  77.07),
+}
