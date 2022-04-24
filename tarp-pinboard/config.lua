4bd8e27f6bd4cbd3fb7655c20b3fbc1f76fbd822
trutzzz <scmvaam@gmail.com>
2022-04-24 22:07:38 +0700

    fix pasang pinboard ada tax/pajak

---
+++ tarp-pinboard/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+Config.webhook = "https://discord.com/api/webhooks/962387046213775390/HxOUpoE3Ilu0S8pHm4Zmqi7B2O4RfaYLMTsSWjugcQvWmtbI9aV4H9XZGyJNLhs46bDA"
+Config.Posters = {
+    [1] = {["city"] = "Valentine", ["coords"] = {-183.73, 630.20, 114.08},},
+    [2] = {["city"] = "Saint Denis", ["coords"] = {2517.57, -1305.57, 48.99},},
+    [3] = {["city"] = "Rhodes", ["coords"] = {1229.85, -1289.56, 76.90},},
+    [4] = {["city"] = "Blackwater", ["coords"] = {-875.133, -1328.75, 43.95},},
+    [5] = {["city"] = "Strawberry", ["coords"] = {-1802.16, -358.23, 163.85},},
+}
